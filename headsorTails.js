5acd8d6eb9f728293ee07e24c687edf89fac1d04
Aydan Abbaslı <130776228+Aydan-A@users.noreply.github.com>
2024-08-27 16:09:48 -0400

    Improved coin animation synchronization

--- headsorTails.js
+++ headsorTails.js
@@ -3,14 +3,19 @@ var btn = document.getElementById("submitButton");
 var coins = document.querySelector(".coins");
 var text = document.querySelector("h1");
 
-
 btn.addEventListener("click", () => {
-
   let result = Math.floor(Math.random() * 2);
+  
+  setTimeout(() => {
+    setTimeout
+    coins.style.animation = result
+    ? "spin-tail 2s forwards" 
+    : "spin-head 2s forwards"
 
-  coins.style.animation = result
-  ? "-webkit-animation: spin-tail 2s forward;" 
-  : "-webkit-animation: spin-head 2s forward;";
-
-  text.textContent = `The coin landed on ${result > 0 ? "tails" : "heads"}`;
+    setTimeout(() => {
+      text.textContent = `The coin landed on ${result > 0 ? "tails" : "heads"}`;
+    }, 2000);
+  
+  }, 100);
+  
 });
